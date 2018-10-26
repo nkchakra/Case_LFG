8a8c326942601a0d0d339c5d0c07148fb8258952
Vishal Patel <vsp20@case.edu>
2018-10-26 16:42:50 -0400

    minor changes

--- front-end/src/Components/Feed.js
+++ front-end/src/Components/Feed.js
@@ -59,17 +59,6 @@ class Feed extends Component {
 
     return (
         <div className="main-feed">
-        <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
-          <Tab eventKey={1} title="Tab 1">
-            Tab 1 content
-          </Tab>
-          <Tab eventKey={2} title="Tab 2">
-            Tab 2 content
-          </Tab>
-          <Tab eventKey={3} title="Tab 3">
-            Tab 3 content
-          </Tab>
-        </Tabs>;
         <style>{`
               *{
                   box-sizing: border-box;
