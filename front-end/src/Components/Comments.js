3217d1ad2dcc1a42386c00c0ec075a5b1e51d260
byrrice <32532726+byrrice@users.noreply.github.com>
2018-12-07 17:53:45 -0500

    Navbar

--- front-end/src/Components/Comments.js
+++ front-end/src/Components/Comments.js
@@ -2,13 +2,54 @@ import React, { Component } from 'react';
 import {ListGroup, ListGroupItem} from 'react-bootstrap';
 
 class Comments extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+            ],
+        }
+    }
+
     handleSubmit(e){
         e.preventDefault();
         console.log("Submitting the form!");
         const author = this.refs.author.value;
         const comment = this.refs.comment.value;
         console.log(author, comment);
+
+        var getRequest = {
+            request: [{
+                queryType: "postComment",
+                comment: comment,
+                username: author,
+                post_id: 1234
+            }]
+        };
+
+        var ws = new WebSocket("ws://ec2-18-191-25-105.us-east-2.compute.amazonaws.com:6009");
+
+        ws.onopen = function() {
+            console.log("sending data..");
+            ws.send(JSON.stringify(getRequest));
+            console.log("sent")
+        };
+
+        ws.onmessage = function (evt) {
+            console.log("anything");
+            console.log(evt.data);
+            this.setState()
+        };
+
+        ws.onclose = function() {
+            alert("Closed!");
+        };
+
+        ws.onerror = function(err) {
+            alert("Error: " + err);
+        };
     }
+
     render(){
         return (
             <div className = "comment">
